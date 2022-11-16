eee3ffb342629da20e9c0e5fd3c69192cc1721a8
hlruffo <hlruffo@gmail.com>
2022-11-16 17:56:09 -0300

    teste de envio bem sucedido

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import Sender from "./sender"
+
+
+const sender = new Sender()
+
---
+++ src/sender.ts
@@ -0,0 +1,38 @@
+import { start } from "repl"
+import { create, Whatsapp, Message, SocketState } from "venom-bot"
+
+class Sender {
+    private client: Whatsapp
+    constructor() {
+        this.initialize()
+
+    }
+
+    async sendText(to: string, body: string) {
+        //formato do to -> 5521982222126@c.us
+        //this.sendText("5521994880549@c.us", "Teste de bot feito por mim ")
+
+        this.client.sendText(to , body)
+
+    }
+
+    private initialize() {
+        const qr = (base64Qrimg: string) => {
+            console.log()
+        }
+
+        const status = (statusSession: string) => {
+        }
+
+        const start = (client: Whatsapp) => {
+            this.client = client
+
+        }
+
+
+        create('suporte', qr, status)
+            .then((client) => start(client))
+            .catch((error) => console.error(error))
+    }
+}
+export default Sender
